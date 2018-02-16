84b9aeeac56ff9a9584a456fa84c79f45e510a17
JoeSuzuki <joesuzuki516@gmail.com>
2018-02-16 11:48:15 -0500

    methods for game square

--- script.js
+++ script.js
@@ -12,4 +12,37 @@ function GameSquare(el, color) {
   this.isOpen = false;
   this.isLocked = false;
   this.el.addEventListener("click", this, false);
+  this.setColor(color); 
+}
+
+// methods called when an event occurs
+GameSquare.prototype.handleEvent = function(e) {
+  switch (e.type) {
+    case "click":
+      if (this.isOpen || this.isLocked) {
+        return;
+      }
+      this.isOpen = true;
+      this.el.classList.add('flip');
+  }
+}
+
+GameSquare.prototype.reset = function() {
+  this.isOpen = false;
+  this.isLocked = false;
+  this.el.classList.remove('flip');
+}
+
+GameSquare.prototype.lock = function() {
+  this.isLocked = true;
+  this.isOpen = true;
+}
+
+// this.el : is the game square div.
+// this.el.children[0] : the game square's child, the "drawer" div.
+// this.el.children[0].children[1] : the second child of the drawer div, this should be the color square.
+GameSquare.prototype.setColor = function(color) {
+  this.el.children[0].children[1].classList.remove(this.color);
+  this.color = color;
+  this.el.children[0].children[1].classList.add(color);
 }
